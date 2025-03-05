f72c5218977c6f7a19b4d5c3b9919878658cbea2
Erika <03erikaaleman@gmail.com>
2025-03-04 21:39:15 -0600

    primer commit del ejercicio tres completo

--- aspx3_19795360/WebForm1.aspx.cs
+++ aspx3_19795360/WebForm1.aspx.cs
@@ -17,7 +17,7 @@ namespace aspx3_19795360
 
         protected void btnConfirm_Click(object sender, EventArgs e)
         {
-            Session["ddlCaytegory"] = ddlCategory.SelectedValue;
+            Session["ddlCategory"] = ddlCategory.SelectedValue;
             Session["ddlSupplier"] = ddlSupplier.SelectedValue;
             Session["strProduct"] = txtProduct.Text;
             Session["strDescription"] = TextArea1.InnerText;
@@ -25,9 +25,9 @@ namespace aspx3_19795360
             Session["decPrice"] = txtPrice.Text;
             Session["bytNumberInStock"] = txtNumberInStock.Text;
             Session["bytNumberOnOrder"] = txtNumberOnOrder.Text;
-            Session["bytRecorrLevel"] = txtReorderLevel.Text;
+            Session["bytReorderLevel"] = txtReorderLevel.Text;
 
-            Response.Redirect("ProductConfirmSessionVariables.aspx");
+            Response.Redirect("WebForm2.aspx");
         }
     }
 }
---
+++ aspx3_19795360/WebForm2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspx3_19795360
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ddlCategory.SelectedValue = Session["ddlCategory"].ToString();
+            ddlSupplier.SelectedValue = Session["ddlSupplier"].ToString();
+            lblProduct.Text = Session["strProduct"].ToString();
+            textArea1.InnerText = Session["strDescription"].ToString();
+            lblImage.Text = Session["strImage"].ToString();
+            Decimal decPrice = Convert.ToDecimal(Session["decPrice"].ToString());
+            lblPrice.Text = decPrice.ToString("C");
+            lblNumberInStock.Text = Session["bytNumberInStock"].ToString();
+            lblNumberOnOrder.Text = Session["bytNumberOnOrder"].ToString();
+            lblReorderLevel.Text = Session["bytReorderLevel"].ToString();
+            Byte bytNumberInStock = Convert.ToByte(Session["bytNumberInStock"]);
+            Byte bytNumberOnOrder = Convert.ToByte(Session["bytNumberOnOrder"]);
+            Decimal decValueInStock = decPrice * bytNumberInStock;
+            Decimal decValueOnOrder = decPrice * bytNumberOnOrder;
+            lblValueInStock.Text = decValueInStock.ToString("C");
+            lblValueOnOrder.Text = decValueOnOrder.ToString("C");
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
